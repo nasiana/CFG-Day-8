4a8a3b4b3a2a7000911c8e049d028640d93440a7
nasiana <87439662+nasiana@users.noreply.github.com>
2021-10-11 00:58:10 +0100

    Add files via upload

--- Lecture.py
+++ Lecture.py
@@ -19,8 +19,13 @@ print('You need {} pizzas for {} friends'.format(pizzas, friends))
 for i in range(5):
     print(i)
 
-# exercise
+# exercise, did myself
 user_word = input("Give me a word")
 for i in range(5):
     user_encoding = "ABCDEF"
-    print(user_encoding.join(user_word))
+    user_encoded = user_encoding.join(user_word)
+
+print(user_encoded)
+
+# just finished for loops, currently at 39:22
+# now going onto while loops
